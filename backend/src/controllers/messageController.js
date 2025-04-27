b805d58974a5929638f3fc010697e3c33c228368
Shreya Gandhari <gandharishreya@gmail.com>
2025-04-28 01:06:39 +0530

    commit done

--- backend/src/controllers/messageController.js
+++ backend/src/controllers/messageController.js
@@ -18,6 +18,7 @@ export const getUsersForSidebar = async (req, res) => {
 
 export const getMessages = async (req, res) => {
   try {
+    console.log("Received ID:", req.params.id);
     const { id: userToChatId } = req.params;
     const myId = req.user._id;
 
