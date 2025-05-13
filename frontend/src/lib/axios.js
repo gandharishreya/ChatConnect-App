c54305bcc18b842788cf4335ab30040a38fa6d52
Shreya Gandhari <gandharishreya@gmail.com>
2025-05-14 02:21:13 +0530

    done

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  //baseURL: import.meta.env.MODE==="development" ? "http://localhost:5001/api" :"/api",
-  baseURL: import.meta.env.MODE==="development" ? "https://chatconnect-app-backend.onrender.com" : "/api",
+  baseURL: import.meta.env.MODE==="development" ? "http://localhost:5001/api" :"/api",
+  //baseURL: import.meta.env.MODE==="development" ? "https://chatconnect-app-backend.onrender.com" : "/api",
   withCredentials: true,
 });
 
